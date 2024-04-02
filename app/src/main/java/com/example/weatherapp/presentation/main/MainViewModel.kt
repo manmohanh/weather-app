9ee8ffed11bf2ab93d670c3a050106f1f45ee5f9
Manmohan Hansda <hmanmohan12@gmail.com>
2024-04-02 16:07:23 +0530

    initial commit

---
+++ app/src/main/java/com/example/weatherapp/presentation/main/MainViewModel.kt
@@ -0,0 +1,8 @@
+package com.example.weatherapp.presentation.main
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+
+@HiltViewModel
+class MainViewModel:ViewModel() {
+}
